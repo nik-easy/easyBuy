688ebb44caa6fa281d2e91de96fe68ae5b1e3226
nikEasy <56231861+nik-easy@users.noreply.github.com>
2019-10-07 00:11:56 +0530

    Update and rename EasuBuyApplication.java to EasyBuyApplication.java

--- main/java/com/easyBuy/EasyBuyApplication.java
+++ main/java/com/easyBuy/EasyBuyApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EasuBuyApplication {
+public class EasyBuyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EasuBuyApplication.class, args);
+		SpringApplication.run(EasyBuyApplication.class, args);
 	}
 }
